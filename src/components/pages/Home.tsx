78a899e9da0347485e48c04cac02846336ba1823
Femi Gabriel <timfemi@gmail.com>
2023-09-05 19:20:18 +0100

    resume fixes

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -6,14 +6,10 @@ import {
   MailFilled,
   TwitterSquareFilled,
 } from "@ant-design/icons";
-import DarkModeToggle from "../DarkModeToggle/DarkModeToggle";
 
 export const Home = () => {
-  
   return (
     <div className="lg:px-10 lg:py-5 lg:mt-20">
-      {/* <DarkModeToggle /> */}
-
       <div className="flex justify-between home-page">
         <div>
           <div className="title lg:mt-20">
@@ -21,11 +17,27 @@ export const Home = () => {
               <span> Hello,</span>
               <span> I'm Jebutu Blessing</span>
             </h1>
+            {/* <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="456"
+              height="35"
+              viewBox="0 0 456 35"
+              fill="none"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M0.983288 34.3491C150.011 10.2181 300.163 7.5906 448.034 22.5824C450.971 22.8809 455.336 20.2951 455.699 16.3061C456.066 12.244 451.974 9.05394 449.03 8.71878C300.048 -8.30674 150.474 0.853601 0.747405 32.192C-0.204279 32.3918 0.0494128 34.5009 0.983288 34.3491Z"
+                fill="#F7DDD4"
+              />
+            </svg> */}
+            <img src="./assets/images/vector.svg" className="vector" alt="" />
           </div>
           <div>
-            <h3 className="text-[#FDFDFD] role font-bold lg:text-[26x] mt-3 mb-10 leading-widest">
+            <h3 className="role font-bold lg:text-[26x]  mb-10 leading-widest">
               Accountant, Finance Officer, Internal Audit/Control
             </h3>
+
             <p className="passage lg:text-[16px] sm:text=[12px] leading-relaxed lg:w-[38em]  mt-5 tracking-relaxed mb-10">
               A Driven accountant with over five years of experience
               specializing in accounting, auditing, budgeting, and performance
